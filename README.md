61f693fb340a9fbde5b63c34417ee727e60d5b9d
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2020-12-09 01:00:39 +0900

    q2の準備

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ s-tパス判定
 ```
 cat inputs/graph_input_st.txt  | cargo run --bin main_st_path
 ```
+
+# q2
+最短経路問題(重みなし)
+
+```
+cat inputs/graph_input_shortest_path.txt  | cargo run --bin main_shortest_path
+```
---
+++ src/bin/main_shortest_path.rs
@@ -0,0 +1,33 @@
+use graph::input_module;
+use graph::graph_module;
+
+fn main() {
+    let input1 = input_module::get_input();
+    let nums = input_module::convert_nums(input1);
+    let node_num = nums[0];
+    let edge_num = nums[1];
+    let mut node_vec = vec![vec![0; 0]; node_num];
+
+    for _i in 0..edge_num {
+        let input_tmp = input_module::get_input();
+        let nums_tmp = input_module::convert_nums(input_tmp);
+        let node1 = nums_tmp[0];
+        let node2 = nums_tmp[1];
+        node_vec[node1].push(node2);
+    }
+    // println!("{:?}", node_vec);
+
+    let input2 = input_module::get_input();
+    let judge_num = input_module::convert_num(input2);
+
+    for _i in 0..judge_num {
+        let input_tmp = input_module::get_input();
+        let nums_tmp = input_module::convert_nums(input_tmp);
+        let from = nums_tmp[0];
+        let to = nums_tmp[1];
+        let visited = graph_module::bfs(from, &node_vec);
+        let result = visited[to] != -1;
+        println!("{}", result);
+    }
+}
+
